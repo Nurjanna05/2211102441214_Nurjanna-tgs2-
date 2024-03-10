341d82bad390fdd74b2dc4261bfb6c03d09ff797
Nurjanna05 <145557813+Nurjanna05@users.noreply.github.com>
2024-03-10 18:59:19 +0800

    summary

---
+++ patterns_codelab/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "E:\\SEMESTER 4\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\User\\patterns_codelab" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=E:\\SEMESTER 4\\flutter"
+  "PROJECT_DIR=C:\\Users\\User\\patterns_codelab"
+  "FLUTTER_ROOT=E:\\SEMESTER 4\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\User\\patterns_codelab\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\User\\patterns_codelab"
+  "FLUTTER_TARGET=C:\\Users\\User\\patterns_codelab\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC8yZTRiYTljNmZiNDk5Y2NkNGU4MTQyMDU0Mzc4M2NjNzI2N2FlNDA2Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\User\\patterns_codelab\\.dart_tool\\package_config.json"
+)
